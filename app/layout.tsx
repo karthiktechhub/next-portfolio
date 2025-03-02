5d0f337e95e52946592047eb664d918496f08ecf
Karthik K R <karthikkhoday29@gmail.com>
2025-03-02 16:53:17 +0530

    uncomment splashcursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <body
         className={`${geist.variable} ${geistMono.variable} bg-background tracking-tight antialiased`}
       >
-        {/* <ConditionalSplashCursor /> */}
+        <ConditionalSplashCursor />
         <Providers>
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
             <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
