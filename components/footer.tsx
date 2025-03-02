6adaa4c3519f3fce39f94b174564d07344e0db8c
Karthik K R <karthikkhoday29@gmail.com>
2025-03-02 16:57:49 +0530

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
         <TextLoop className="text-muted-foreground text-xs">
-          <span>© 2025 Karthik.</span>
+          <span>© 2025 Dhekhyo.</span>
           <span>Built with ❤️</span>
         </TextLoop>
         <div className="">
